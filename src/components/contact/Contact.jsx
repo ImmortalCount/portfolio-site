d18e1cbbfab65cfe07b1ce586dd382efc96c62fc
ImmortalCount <devnaut@gmail.com>
2022-09-15 11:13:21 -0700

    email working

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
+import emailjs from '@emailjs/browser';
 import "./contact.scss";
 
 export default function Contact() {
+  const form = useRef();
   const [message, setMessage] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessage(true);
+
+    //order, YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, YOUR_PUBLIC_KEY
+    emailjs.sendForm('service_ymg9slm', 'template_cduncr8', form.current, '1NU5Mi5w3MFel77Fc')
+      .then((result) => {
+          console.log(result.text);
+          setMessage(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+
   };
   return (
     <div className="contact" id="contact">
@@ -15,11 +26,15 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        {message && <span>Thanks, you'll be hearing from me soon :)</span>}
+        <form ref={form} onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input type="text" name="from_name" />
+        <label>Email</label>
+        <input type="email" name="from_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />   
         </form>
       </div>
     </div>
