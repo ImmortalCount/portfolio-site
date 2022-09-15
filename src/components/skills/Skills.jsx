4408684d7c370bf933551e2974901d24a6888f11
ImmortalCount <devnaut@gmail.com>
2022-09-14 17:04:10 -0700

    skill section redesign

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,18 +6,18 @@ export default function Skills() {
     <div className='skills'>
         <h1 className='header'>Skills</h1>
     <div className='skill_items'>
-        <div className='skill_item'><img src='assets/skills/html5.png' alt=''/>HTML</div>
-        <div className='skill_item'><img src='assets/skills/css.png' alt=''/>CSS</div>
-        <div className='skill_item'><img src='assets/skills/javascript.png' alt=''/>Javascript</div>
-        <div className='skill_item'><img src='assets/skills/sass.png' alt=''/>SCSS</div>
-        <div className='skill_item'><img src='assets/skills/git.png' alt=''/>Git</div>
-        <div className='skill_item'><img src='assets/skills/react.png' alt=''/>React</div>
-        <div className='skill_item'><img src='assets/skills/redux.png' alt=''/>Redux</div>
-        <div className='skill_item'><img src='assets/skills/node-js.png' alt=''/>Node</div>
-        <div className='skill_item'><img src='assets/skills/express.png' alt=''/>Express</div>
-        <div className='skill_item'><img src='assets/skills/python.png' alt=''/>Python</div>
-        <div className='skill_item'><img src='assets/skills/firebase.png' alt=''/>Firebase</div>
-        <div className='skill_item'><img src='assets/skills/mongodb.png' alt=''/>Mongodb</div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/html5.png' alt=''/><div>HTML</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/css.png' alt=''/><div>CSS</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/javascript.png' alt=''/><div>Javascript</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/sass.png' alt=''/><div>SCSS</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/git.png' alt=''/><div>Git</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/react.png' alt=''/><div>React</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/redux.png' alt=''/><div>Redux</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/node-js.png' alt=''/><div>Node</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/express.png' alt=''/><div>Express</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/python.png' alt=''/><div>Python</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/firebase.png' alt=''/><div>Firebase</div></div>
+        <div className='skill_item'><img className='skill_image' src='assets/skills/mongodb.png' alt=''/><div>MongoDB</div></div>
     </div>
     </div>
   )
